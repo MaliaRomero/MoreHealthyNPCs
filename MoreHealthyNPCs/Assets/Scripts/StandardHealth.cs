1a9550a8596f1df76a99733079926e2f99323a3f
MaliaRomero <romerm15@my.erau.edu>
2024-11-12 20:20:21 -0700

    Update

--- MoreHealthyNPCs/Assets/Scripts/StandardHealth.cs
+++ MoreHealthyNPCs/Assets/Scripts/StandardHealth.cs
@@ -5,6 +5,8 @@ public class StandardHealth : MonoBehaviour, IHealth
 {
     [SerializeField] private int startingHealth = 100;
 
+    public GameObject crown;
+
     private int currentHealth;
 
     public event Action<float> OnHPPctChanged = delegate { };
@@ -21,16 +23,28 @@ public class StandardHealth : MonoBehaviour, IHealth
     }
 
     public void TakeDamage(int amount)
-    {
-        if (amount <= 0)
-            throw new ArgumentOutOfRangeException("Invalid Damage amount specified: " + amount);
+    {   
+        if (startingHealth >= 1000)
+        {
+            if (crown != null)
+            {
+                crown.SetActive(!crown.activeSelf);
+            } else {
+                crown.SetActive(true);
+            }
+        } 
+        else 
+        {
+            if (amount <= 0)
+                throw new ArgumentOutOfRangeException("Invalid Damage amount specified: " + amount);
 
-        currentHealth -= amount;
+            currentHealth -= amount;
 
-        OnHPPctChanged(CurrentHpPct);
+            OnHPPctChanged(CurrentHpPct);
 
-        if (CurrentHpPct <= 0)
-            Die();
+            if (CurrentHpPct <= 0)
+                Die();            
+        }
     }
 
     private void Die()
